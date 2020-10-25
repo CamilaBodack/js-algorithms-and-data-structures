1a83f00d58e79960b23612786141fe5630c0295c
CamilaBodack <camila.bodack@gmail.com>
2020-10-25 14:03:48 -0300

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # js-algorithms-and-data-structures
+
+Project containing my FreeCodeCamp JavaScript Algorithms And Data Structures challenges's solutions.
--- src/basic-algorithm-scripting/title-case-a-sentence.js
+++ src/basic-algorithm-scripting/title-case-a-sentence.js
@@ -1,3 +1,9 @@
+/*Title Case a Sentence
+
+Return the provided string with the first letter of each word capitalized. Make sure the rest of the word is in lower case.
+
+For the purpose of this exercise, you should also capitalize connecting words like "the" and "of".*/
+
 function titleCase(str) {
     let splitArr = str.split(" ")
     let eachWord = []
