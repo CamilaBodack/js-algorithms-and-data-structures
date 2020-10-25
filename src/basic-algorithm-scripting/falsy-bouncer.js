1681b98aa2c5293b6dee1f779e344a444c024fc6
CamilaBodack <camila.bodack@gmail.com>
2020-10-25 18:06:44 -0300

    solve falsy-bouncer

---
+++ src/basic-algorithm-scripting/falsy-bouncer.js
@@ -0,0 +1,19 @@
+/*
+
+Remove all falsy values from an array.
+Falsy values in JavaScript are false, null, 0, "", undefined, and NaN.
+Hint: Try converting each value to a Boolean.
+
+*/
+
+function bouncer(arr) {
+  for(let position = 0; position < arr.length; position ++){
+    if(Boolean(arr[position]) !== true){
+      arr.splice(position, 1)
+      position--
+    }
+  }
+  return arr;
+}
+
+bouncer([false, null, 0, NaN, undefined, ""]);
