1e2a55f4363218c8e8f01126dbf14d787efae74a
CamilaBodack <camila.bodack@gmail.com>
2020-10-25 10:19:12 -0300

    create function boolean

---
+++ src/basic-algorithm-scripting/boo-who.js
@@ -0,0 +1,10 @@
+/* Scripting: Boo who
+Check if a value is classified as a boolean primitive. Return true or false.
+Boolean primitives are true and false. */
+
+function booWho(bool) {
+  if(typeof bool === 'boolean'){return true}
+  else{return false}
+}
+
+booWho(false);
