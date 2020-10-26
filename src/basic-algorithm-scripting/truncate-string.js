02c8535bfa23c7e5affc2f9d00ad38e8fb9853fc
CamilaBodack <camila.bodack@gmail.com>
2020-10-26 11:42:44 -0300

    solve where-do-i-belong array insert and sort problem

--- src/basic-algorithm-scripting/truncate-string.js
+++ src/basic-algorithm-scripting/truncate-string.js
@@ -1,9 +1,6 @@
 function truncateString(str, num) {
-  if(str.length > num){
-    return str.substring(0, num).concat('...')
-  }else{
-    return str
-  }
+  if(str.length > num){ return str.substring(0, num).concat('...')}
+  else{return str}
 }
 
 truncateString("A-", 1);
