96c405ff3e932d157156ee753b3683f8268a56e9
CamilaBodack <camila.bodack@gmail.com>
2020-10-27 16:21:00 -0300

    some usage to check positive numbers from arr

---
+++ src/functional-programming/some-usage.js
@@ -0,0 +1,11 @@
+/*
+Use the some method inside the checkPositive function to check if any element in arr is positive. The function should return a Boolean value.
+*/
+
+function checkPositive(arr) {
+  // Only change code below this line
+return arr.some(number => number >= 0)
+
+  // Only change code above this line
+}
+checkPositive([1, 2, 3, -4, 5]);
