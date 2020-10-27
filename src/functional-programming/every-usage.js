3f878b5cb6f6ff9d7e329a308aef8e1a3989e6a4
CamilaBodack <camila.bodack@gmail.com>
2020-10-27 16:18:13 -0300

    usage of every to solve positive numbers challenge

---
+++ src/functional-programming/every-usage.js
@@ -0,0 +1,11 @@
+/*
+Use the every method inside the checkPositive function to check if every element in arr is positive. The function should return a Boolean value.
+*/
+
+function checkPositive(arr) {
+  // Only change code below this line
+if(!arr.every(number => number >= 0)){return false}
+else{return true}
+  // Only change code above this line
+}
+checkPositive([1, 2, 3, -4, 5]);
