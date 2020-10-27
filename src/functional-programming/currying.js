8e13af997b983b0bc58600ca168aab6909f01a52
CamilaBodack <camila.bodack@gmail.com>
2020-10-27 16:27:36 -0300

    solve currying challenge

---
+++ src/functional-programming/currying.js
@@ -0,0 +1,14 @@
+/*
+Fill in the body of the add function so it uses currying to add parameters x, y, and z.
+*/
+
+function add(x) {
+    // Only change code below this line
+    return function(y) {
+        return function(z) {
+            return x + y + z
+        }
+    }
+    // Only change code above this line
+}
+add(10)(20)(30);
