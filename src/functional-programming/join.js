dedbeb37f03c2b4182c6479a7a1367790dc49736
CamilaBodack <camila.bodack@gmail.com>
2020-10-27 15:30:30 -0300

    solve join challenge using regex to split and join by spaces

---
+++ src/functional-programming/join.js
@@ -0,0 +1,12 @@
+/*
+Use the join method (among others) inside the sentensify function to make a sentence from the words in the string str. The function should return a string. For example, "I-like-Star-Wars" would be converted to "I like Star Wars". For this challenge, do not use the replace method.
+*/
+
+function sentensify(str) {
+  // Only change code below this line
+  let newArr = str.split(/[\-\.\,]/)
+  return newArr.join(" ")
+
+  // Only change code above this line
+}
+sentensify("May-the-force-be-with-you");
