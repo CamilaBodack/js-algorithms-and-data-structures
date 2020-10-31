c90a1687c12c693e90c0980b12d8dc6335d8b0c4
CamilaBodack <camila.bodack@gmail.com>
2020-10-31 19:22:39 -0300

    add pig-latin solution

---
+++ src/intermediate-algorithm-scripting/pig-latin.js
@@ -0,0 +1,36 @@
+/*
+Pig Latin is a way of altering English Words. The rules are as follows:
+
+- If a word begins with a consonant, take the first consonant or consonant cluster, move it to the end of the word, and add "ay" to it.
+
+- If a word begins with a vowel, just add "way" at the end.
+
+Translate the provided string to Pig Latin. Input strings are guaranteed to be English words in all lowercase.
+*/
+
+function translatePigLatin(str) {
+    let newArr = [...str]
+    let counter = 0
+    let noVowel = newArr.every(letter => (!letter.match(/[aeiou]/)))
+
+    if (noVowel) {
+        newArr.push("ay")
+        return newArr.join("")
+    }
+
+    if (str[0].match(/[aeiou]/)) {
+        newArr.push("way")
+        return newArr.join("")
+    }
+
+    while (!str[counter].match(/[aeiou]/) && counter < newArr.length) {
+        newArr.shift()
+        newArr.push(str[counter])
+        counter++
+    }
+    newArr.push("ay")
+    console.log(newArr.join(""))
+    return newArr.join("")
+}
+
+translatePigLatin("rhythm")
