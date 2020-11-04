9d462c3995213530e6f14b3ae939fce69851e067
CamilaBodack <camila.bodack@gmail.com>
2020-11-04 16:02:34 -0300

    add challenge description in binary-agents

--- src/intermediate-algorithm-scripting/binary-agents.js
+++ src/intermediate-algorithm-scripting/binary-agents.js
@@ -1,16 +1,24 @@
+/*
+Binary Agents
+
+Return an English translated sentence of the passed binary string.
+
+The binary string will be space separated.
+*/
+
 function binaryAgent(str) {
-  let splitStr = str.split(" ")
-  let arrDec = []
-  let newString = []
+    let splitStr = str.split(" ")
+    let arrDec = []
+    let newString = []
 
-  for(let i = 0; i < splitStr.length; i++){
-    arrDec.push(parseInt(splitStr[i], 2).toString(10))
-    for(let j = 0; j < 1; j++){
-      newString.push(String.fromCharCode(arrDec[i]))
+    for (let i = 0; i < splitStr.length; i++) {
+        arrDec.push(parseInt(splitStr[i], 2).toString(10))
+        for (let j = 0; j < 1; j++) {
+            newString.push(String.fromCharCode(arrDec[i]))
+        }
     }
-  }
 
- return newString.join("")
+    return newString.join("")
 }
 
 binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111");
