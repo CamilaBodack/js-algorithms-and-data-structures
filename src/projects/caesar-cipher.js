46893dacfa70123f504cfd3c3af6654c516f723a
CamilaBodack <camila.bodack@gmail.com>
2020-11-06 17:27:02 -0300

    add caesar cipher initial solution structure

---
+++ src/projects/caesar-cipher.js
@@ -0,0 +1,19 @@
+function rot13(str) {
+
+let rot13Split = str.replaceAll(" ", "").split("")
+let letterIndex
+let decoded = []
+const alphabet =
+[
+"A","B","C","D","E","F","G","H","I","J","K", "L","M","N","O","P","Q","R",'S',"T","U","V","W","X","Y","Z"
+]
+
+console.log(alphabet.indexOf('S'))
+console.log(alphabet[5])
+for(let i = 0; i < alphabet.length; i++){
+  letterIndex = alphabet.indexOf('S')
+
+}
+}
+
+rot13("SERR PBQR PNZC");
