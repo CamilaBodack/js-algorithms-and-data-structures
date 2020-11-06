4e1751f60a026b5bc544294af1a750de03b76777
CamilaBodack <camila.bodack@gmail.com>
2020-11-06 15:05:32 -0300

    create other method to solve roman numbers converter

--- src/projects/roman-numeral-converter.js
+++ src/projects/roman-numeral-converter.js
@@ -8,33 +8,10 @@ All roman numerals answers should be provided in upper-case.
 
 
 function convertToRoman(num) {
-let tenMod = num % 10
-let fiveMod = num % 5
-let dec = (num - tenMod)/10
-let fiftyMod = num % 50 // <= 50
-let centMod = num/100 // >= 1 pra validar
-let fivMod = num % 500 // <= 500
-let milMod = num/1000 // >=  1 pra validar
-let romanArr = []
-
-let romanNumber = [
-    {
-    1: "I",
-    5: "V",
-    10: "X",
-    50: "L",
-    100: "C",
-    500: "D",
-    1000: "M"
-    }
-    ]
-
-console.log('---', tenMod)
-for(let i = 0; i < romanNumber.length; i++){
-    romanArr.push(romanNumber[i][1]*tenMod)
+    const unidade = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
+    const dezena = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]
+    const centena = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
+    const milhar = ["", "M", "MM", "MMM"]
 }
 
-console.log(romanArr)
-}
-
-convertToRoman(2014);
+convertToRoman(36);
