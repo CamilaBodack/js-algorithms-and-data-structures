c9856c43d0a2cd345935949cddb9db07f9c7fcc0
CamilaBodack <camila.bodack@gmail.com>
2020-11-06 16:00:16 -0300

    project roman numeral converter solution

--- src/projects/roman-numeral-converter.js
+++ src/projects/roman-numeral-converter.js
@@ -8,10 +8,18 @@ All roman numerals answers should be provided in upper-case.
 
 
 function convertToRoman(num) {
+
     const unidade = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
     const dezena = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]
     const centena = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
     const milhar = ["", "M", "MM", "MMM"]
+
+    let u = num % 10
+    let d = (num % 100 - u) / 10
+    let c = parseInt(((num % 1000) / 100))
+    let m = parseInt(((num / 1000) - c / 10))
+
+    return milhar[m] + centena[c] + dezena[d] + unidade[u]
 }
 
-convertToRoman(36);
+convertToRoman(563);
