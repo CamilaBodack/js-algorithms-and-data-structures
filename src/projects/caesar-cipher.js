123ad2961822d7a4134046d6ce33651757eca2c3
CamilaBodack <camila.bodack@gmail.com>
2020-11-06 18:14:02 -0300

    solve project 3 caesar-cipher

--- src/projects/caesar-cipher.js
+++ src/projects/caesar-cipher.js
@@ -1,19 +1,38 @@
 function rot13(str) {
 
-let rot13Split = str.replaceAll(" ", "").split("")
+let rot13Split = str.split("")
 let letterIndex
+let rot13
+let toDecoded = []
 let decoded = []
+const chars = [" ", "!", ",", ".", "?"]
+
 const alphabet =
 [
 "A","B","C","D","E","F","G","H","I","J","K", "L","M","N","O","P","Q","R",'S',"T","U","V","W","X","Y","Z"
 ]
 
-console.log(alphabet.indexOf('S'))
-console.log(alphabet[5])
-for(let i = 0; i < alphabet.length; i++){
-  letterIndex = alphabet.indexOf('S')
+for(let i = 0; i < rot13Split.length; i++){
+  if(chars.includes(rot13Split[i])){
+    toDecoded.push(rot13Split[i])
+    continue
+  }
+  letterIndex = alphabet.indexOf(rot13Split[i])
+  rot13 = (letterIndex + 13) % 26
+  toDecoded.push(rot13)
+}
+
+for(let j = 0; j < toDecoded.length; j++){
+  if(chars.includes(toDecoded[j])){
+    decoded.push(toDecoded[j])
+    continue
+  }
+      decoded.push(alphabet[toDecoded[j]])
+}
+
+return decoded.join("")
 
 }
-}
 
+rot13("SERR CVMMN!")
 rot13("SERR PBQR PNZC");
