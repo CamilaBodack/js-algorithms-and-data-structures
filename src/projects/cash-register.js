75836c3d055c49591a35bf5ceeea302275e2e67b
CamilaBodack <camila.bodack@gmail.com>
2020-11-08 14:21:14 -0300

    add validation if insuficcient funds in cid

--- src/projects/cash-register.js
+++ src/projects/cash-register.js
@@ -1,7 +1,11 @@
 function checkCashRegister(price, cash, cid) {
 
- let sumCid = cid.map(pos => pos[1]).reduce((posA, posB) => posA + posB).toFixed(2)
+let sumCid = cid.map(coin => coin[1]).reduce((valueA, valueB) => valueA + valueB).toFixed(2)
 
+if(cash > sumCid){
+  return  {status: "INSUFFICIENT_FUNDS", change: []}
+  }
 }
+
 
 checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
