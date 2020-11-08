14b0aaa42bfda0229a6ff886858b010199cc4a33
CamilaBodack <camila.bodack@gmail.com>
2020-11-08 12:28:21 -0300

    solve project #4 us phone number validator using regex

--- src/projects/telephone-number-validator.js
+++ src/projects/telephone-number-validator.js
@@ -6,11 +6,12 @@ The user may fill out the form field any way they choose as long as it has the f
 */
 
 function telephoneCheck(str) {
-    let regexPhone = /1?[\s]?[(]?[\d]{3}[)]?[-]?[\s]?[\d]{3}[-]?[\s]?[\d]{4}/
-    if (str.match(regexPhone)) {
-        return true
-    }
-    return false
+  let regexPhone = /^(1\s?)?((\([\d]{3}\))|[\d]{3})[\s\-]?[\d]{3}[\s\-]?[\d]{4}$/
+  if(str.match(regexPhone)){
+    return true
+  }
+  return false
 }
 
 telephoneCheck("555-555-5555");
+telephoneCheck("1 555)555-5555")
