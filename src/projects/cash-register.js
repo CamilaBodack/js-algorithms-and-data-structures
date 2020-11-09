9bab78e4ca527af09e686f8102ef6dd67edba8d5
CamilaBodack <camila.bodack@gmail.com>
2020-11-09 18:39:53 -0300

    adjust cid array derivates in way it makes the correct reverse derivation

--- src/projects/cash-register.js
+++ src/projects/cash-register.js
@@ -1,15 +1,14 @@
 function checkCashRegister(price, cash, cid) {
 
 let sumCid = Number(cid.map(coin => coin[1]).reduce((valueA, valueB) => valueA + valueB)).toFixed(1)
-let sortCid = cid.sort((a, b) => a + b)
+let sortCid = cid.sort((a, b) => a - b)
 let movArr = []
 let result = 0
 let change = (cash - price)
 
 
 if(change == sumCid){
-  return {status: "CLOSED", change: [...sortCid]}
-
+  return {status: "CLOSED", change: sortCid.reverse()}
 }
 
 if(cash > sumCid){
@@ -27,8 +26,9 @@ newCidArr[6].unshift(.10)
 newCidArr[7].unshift(.05)
 newCidArr[8].unshift(.01)
 
-
+console.log(newCidArr)
 for(let i = 0; i < newCidArr.length; i++){
+  console.log(newCidArr)
   if(newCidArr[i][0] <= change){
     if(change % newCidArr[i][0] == 0){
       movArr.push(newCidArr[i][1], change)
@@ -39,5 +39,4 @@ for(let i = 0; i < newCidArr.length; i++){
 
 }
 
-
-checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]])
+checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]])
