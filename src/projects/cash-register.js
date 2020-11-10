416ab60016961df661b1ad462bba116f75156072
CamilaBodack <camila.bodack@gmail.com>
2020-11-10 15:10:05 -0300

    try to solve position in array

--- src/projects/cash-register.js
+++ src/projects/cash-register.js
@@ -3,17 +3,16 @@ function checkCashRegister(price, cash, cid) {
 let sumCid = Number(cid.map(coin => coin[1]).reduce((valueA, valueB) => valueA + valueB)).toFixed(1)
 let sortCid = cid.sort((a, b) => a - b)
 let movArr = []
-let result = 0
 let change = (cash - price)
-let penny // ((0.01*100)*1.01*100)
-let nickel  //Number(2.05/0.05).toFixed(1)
-let dime //Number(3.1/0.1).toFixed(1)
-let quarter //4.25/0.25)
-let dollar //90/1
-let five //55/5
-let ten // 20/10
-let twenty //60/20
-let one_hundred // 100/100
+let penny
+let nickel
+let dime
+let quarter
+let dollar
+let five
+let ten
+let twenty
+let one_hundred
 
 
 // cash-in-drawer = change
@@ -27,54 +26,52 @@ if(cash > sumCid){
   }
 
 // change
-result = change
-
 one_hundred = (change - cid[0][1])
 
 if(one_hundred > 0){movArr.push(cid[0][1], one_hundred), change = one_hundred}
 twenty = (change - Number((change % cid[1][1])))
 change = Number((change % cid[1][1]).toFixed(2))
 
-
-if(twenty > 0){movArr.push(cid[1][0], twenty)}
+if(twenty > 0){
+movArr.push(cid[1][0], twenty)}
 ten = (change - Number((change % cid[2][1])))
 change = Number((change % cid[2][1]).toFixed(2))
 
 
 if(ten > 0){movArr.push(cid[2][0], ten)}
-five = (change - Number(change % 5).toFixed(2))
+five = (change - Number(change % 5))
 change = Number((change % 5).toFixed(2))
 
 
-
-if(five > 0){movArr.push(cid[3][0], five), result = five}
-dollar = (result - cid[3][1])
-console.log('--dollar',cid[4][0], cid[4][1])
-
-console.log('five', five)
-console.log('change', change.toFixed(2))
+if(five > 0){movArr.push(cid[3][0], five)}
+dollar = (change - Number(change % 1))
+change = Number((change % 1).toFixed(2))
 
 
-
+if(dollar > 0){movArr.push(cid[4][0], dollar)}
+quarter = (change - Number(change % 0.25))
+change = Number((change % 0.25).toFixed(2))
 
 
 
+if(quarter > 0){movArr.push(cid[5][0], quarter)}
+dime = (change - Number(change % 0.1))
+change = Number((change % 0.1).toFixed(2))
 
-if(dollar > 0){movArr.push(cid[4][0], result), result = dollar}
-quarter = (result - cid[5][1])
-console.log('--quarter',cid[5][0], cid[5][1])
-if(quarter > 0){movArr.push(cid[5][0], result), result = quarter}
-dime = (result - cid[6][1])
-console.log('--dime',cid[6][0], cid[6][1])
-if(dime > 0){movArr.push(cid[6][0], result), result = dime}
-nickel = (result - cid[7][1])
-console.log('--nickel',cid[7][0], cid[7][1])
-if(nickel > 0){movArr.push(cid[7][0], result), result = nickel}
-penny = (result - cid[8][1])
-console.log('--penny',cid[8][0], cid[8][1])
-if(penny > 0){movArr.push(cid[8][0], result), result = penny}
 
-//return console.log({status: "OPEN", change: [movArr]})
+if(dime > 0){movArr.push(cid[6][0], dime)}
+nickel = (change - Number(change % 0.05))
+change = Number((change % 0.05).toFixed(2))
+
+
+if(nickel > 0){movArr.push(cid[7][0], nickel)}
+penny = (change - Number(change % 0.01))
+change = Number((change % 0.01).toFixed(2))
+
+
+if(penny > 0){movArr.push(cid[8][0], penny)}
+
+return console.log({status: "OPEN", change: [movArr]})
 
 }
 
